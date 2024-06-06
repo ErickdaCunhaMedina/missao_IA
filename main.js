8353412c6d61446f227f2dd02de11c4fe0e88f6d
ErickdaCunhaMedina <106669357+ErickdaCunhaMedina@users.noreply.github.com>
2024-06-06 11:01:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativos');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
